f4ab5e7491922daebc177dc2464614a7b1109836
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-03-04 21:03:34 +0530

    Update index.js

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -25,9 +25,9 @@ app.use(cors(
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../FRONTEND/dist")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
+        res.sendFile(path.resolve(__dirname,"../FRONTEND","dist","index.html"));
     });
 }
 server.listen(5001,()=>{
